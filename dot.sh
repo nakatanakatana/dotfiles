035e79e5d080a48eb40170e7c6c46b6f5dae7557
tanaka <nakatanakatana@gmail.com>
2019-02-13 23:21:42 +0900

    初期設定スクリプトの設定変更　

--- dot.sh
+++ dot.sh
@@ -6,7 +6,7 @@ ln -s -i $PWD/_gitconfig $HOME/.gitconfig
 ln -s -i $PWD/_profile $HOME/.profile
 ln -s -i $PWD/_screenrc $HOME/.screenrc
 ln -s -i $PWD/_screenrc.ssh_keybind $HOME/.screenrc.ssh_keybind
-ln -s -i $PWD/_vimrc $HOME/.vimrc
+ln -s -i $PWD/nvim/init.vim $HOME/.vimrc
 ln -s -i $PWD/nvim $HOME/.config/nvim
 ln -s -i $PWD/git-tmp $HOME/.git-tmp
 
