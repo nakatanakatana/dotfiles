38731ab4d2e052646af2eff7f3373f6bc1fa1295
tanaka <nakatanakatana@gmail.com>
2020-02-12 01:10:16 +0900

    update

--- dot.sh
+++ dot.sh
@@ -3,8 +3,7 @@ ln -s -i $PWD/_bashrc $HOME/.bashrc
 ln -s -i $PWD/alacritty.yml $HOME/.alacritty.yml
 ln -s -i $PWD/_gitconfig $HOME/.gitconfig
 ln -s -i $PWD/_profile $HOME/.profile
-ln -s -i $PWD/_screenrc $HOME/.screenrc
-ln -s -i $PWD/_screenrc.ssh_keybind $HOME/.screenrc.ssh_keybind
+ln -s -i $PWD/.tmux.conf $HOME/
 ln -s -i $PWD/nvim/init.vim $HOME/.vimrc
 ln -s -i $PWD/nvim $HOME/.config/nvim
 ln -s -i $PWD/git-tmp $HOME/.git-tmp
