9d1e36acb8ba30f962fa0c17035a3dc49ed62ab8
tanaka <nakatanakatana@gmail.com>
2020-02-12 01:13:19 +0900

    rename files

--- dot.sh
+++ dot.sh
@@ -1,8 +1,8 @@
 #! /bin/sh
-ln -s -i $PWD/_bashrc $HOME/.bashrc
+ln -s -i $PWD/.profile $HOME/
+ln -s -i $PWD/.bashrc $HOME/
 ln -s -i $PWD/alacritty.yml $HOME/.alacritty.yml
 ln -s -i $PWD/_gitconfig $HOME/.gitconfig
-ln -s -i $PWD/_profile $HOME/.profile
 ln -s -i $PWD/.tmux.conf $HOME/
 ln -s -i $PWD/nvim/init.vim $HOME/.vimrc
 ln -s -i $PWD/nvim $HOME/.config/nvim
