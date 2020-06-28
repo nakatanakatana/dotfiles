f34a9dc9e7cc68a98b98312805f1f7551f8ed7a2
tanaka <nakatanakatana@gmail.com>
2020-06-28 21:01:22 +0900

    update

--- link.sh
+++ link.sh
@@ -1,11 +1,10 @@
 #! /bin/sh
-ln -s -i $PWD/.profile $HOME/
+# ln -s -i $PWD/.profile $HOME/
 ln -s -i $PWD/.bashrc $HOME/
 ln -s -i $PWD/alacritty.yml $HOME/.alacritty.yml
 ln -s -i $PWD/_gitconfig $HOME/.gitconfig
 ln -s -i $PWD/.tmux.conf $HOME/
 ln -s -i $PWD/nvim/init.vim $HOME/.vimrc
 ln -s -i $PWD/nvim $HOME/.config/nvim
-ln -s -i $PWD/git-tmp $HOME/.git-tmp
 
 git config --global init.templatedir '~/.git-tmp'
