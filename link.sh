1acbe55a0e327b6e4a7d41b1d2753bd6e7babc6a
tanaka <nakatanakatana@gmail.com>
2020-07-07 16:23:10 +0900

    add gitmux.conf

--- link.sh
+++ link.sh
@@ -4,7 +4,6 @@ ln -s -i $PWD/.bashrc $HOME/
 ln -s -i $PWD/alacritty.yml $HOME/.alacritty.yml
 ln -s -i $PWD/_gitconfig $HOME/.gitconfig
 ln -s -i $PWD/.tmux.conf $HOME/
+ln -s -i $PWD/.gitmux.conf $HOME/.gitmux.conf
 ln -s -i $PWD/nvim/init.vim $HOME/.vimrc
 ln -s -i $PWD/nvim $HOME/.config/nvim
-
-git config --global init.templatedir '~/.git-tmp'
