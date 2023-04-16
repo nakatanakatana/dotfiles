ade3838ae432cae22611149e8b8c20bb2faa52bd
tanaka <nakatanakatana@gmail.com>
2023-04-16 06:02:20 +0000

    update

--- link.sh
+++ link.sh
@@ -1,7 +1,6 @@
 #! /bin/sh
 # ln -s -i $PWD/.profile $HOME/
 ln -s -i $PWD/.bashrc $HOME/
-ln -s -i $PWD/alacritty.yml $HOME/.alacritty.yml
 ln -s -i $PWD/_gitconfig $HOME/.gitconfig
 ln -s -i $PWD/.tmux.conf $HOME/
 ln -s -i $PWD/.gitmux.conf $HOME/.gitmux.conf
