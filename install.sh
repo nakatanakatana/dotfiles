478a8606d3caf6822926b400b10353159b059504
tanaka <nakatanakatana@gmail.com>
2025-06-30 01:51:09 +0900

    update

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-curl -sSfL https://raw.githubusercontent.com/aquaproj/aqua-installer/v3.1.1/aqua-installer | bash
+curl -sSfL https://raw.githubusercontent.com/aquaproj/aqua-installer/v4.0.2/aqua-installer | bash
 export PATH=$PATH:${AQUA_ROOT_DIR:-${XDG_DATA_HOME:-$HOME/.local/share}/aquaproj-aqua}/bin
 export AQUA_GLOBAL_CONFIG=$HOME/aqua.yaml
 aqua i -l
